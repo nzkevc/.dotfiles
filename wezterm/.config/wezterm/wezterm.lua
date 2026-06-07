a08a41d6f6127425c8c7360a52ad232a894a759c
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2026-06-07 17:38:58 +1200

    configure wezterm ctrl-backspace to delete word

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,20 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = wezterm.config_builder()
 local mux = wezterm.mux
 
+-- config.default_cursor_style = 'SteadyBar'
 config.enable_tab_bar = false
 config.font = wezterm.font("Cascadia Code NF")
 config.font_size = 14.0
+config.keys = {
+  {
+    -- make ctrl-backspace send ctrl-w
+    key = 'Backspace', 
+    mods = 'CTRL', 
+    action = act.SendKey { key = 'w', mods = 'CTRL' }
+  },
+}
 config.tab_and_split_indices_are_zero_based = true
 config.window_background_opacity = 0.8
 
